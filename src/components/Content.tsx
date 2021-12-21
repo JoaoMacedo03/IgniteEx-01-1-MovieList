ebfc22529985f617c7762be2895c757763144a9a
Joao Macedo <joaovictormacedo91@gmail.com>
2021-12-21 02:54:24 -0300

    Feat: Content Component
    I've created this component to separete the code responsability. Too
    many things were being handled by the App component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard'
+
+interface MovieRating {
+  Value: string
+  Source: string
+}
+
+interface Movies {
+  imdbID: string
+  Title: string
+  Poster: string
+  Runtime: string
+  Ratings: Array<MovieRating>
+}
+
+interface ContentProps {
+  selectedGenre: {
+    title: string
+  },
+  movies: Array<Movies>
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <>
+    <header>
+      <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {props.movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+    </>
+  )
 }
